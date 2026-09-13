d26b3ea56c784a9876b43c167863d553a24e2a4b
Vadim <shilin.vadim.alekseyevich@gmail.com>
2026-09-13 20:22:29 +0300

    Phase 16: apply staged project migration before desktop Core boot

--- apps/desktop/main/companion-desktop-host.js
+++ apps/desktop/main/companion-desktop-host.js
@@ -4,6 +4,7 @@ const { DesktopHost } = require("./desktop-host.js");
 const { ensureDesktopPaths } = require("./app-data.js");
 const { CompanionServerTransport } = require("./companion-server-transport.js");
 const { bindCompanionAgentRuntime } = require("./companion-host-binding.js");
+const { applyPendingCompanionMigration } = require("./companion-migration.js");
 const { CompanionRpcPeer } = require("../../../platform/companion-rpc.js");
 const { DesktopBridgeAgentRuntime } = require("../../../platform/desktop-bridge-runtime.js");
 
@@ -12,6 +13,7 @@ class CompanionDesktopHost extends DesktopHost {
     if (typeof options.agentRuntime?.bindHostHandlers !== "function") throw new TypeError("companion_agent_runtime_required");
     super({ ...options, autoSeedFakeLead: false });
     this.companionUnbind = bindCompanionAgentRuntime(this);
+    this.migrationBootResult = options.migrationBootResult || null;
   }
 
   async close() {
@@ -23,10 +25,22 @@ class CompanionDesktopHost extends DesktopHost {
 
 async function createNativeCompanionDesktopHost({ dataDirectory = null, paths = null, logger = null, requestTimeoutMs = 15000, ...options } = {}) {
   const resolvedPaths = paths || ensureDesktopPaths({ dataDirectory });
+  const clock = options.clock || (() => Date.now());
+  const migrationBootResult = await applyPendingCompanionMigration({
+    paths: resolvedPaths,
+    clock,
+    logger: logger || console
+  });
+  if (!migrationBootResult.ok) {
+    const error = new Error(migrationBootResult.reason || "companion_migration_boot_failed");
+    error.migration = migrationBootResult;
+    throw error;
+  }
+
   const transport = new CompanionServerTransport({ paths: resolvedPaths, logger: logger || console });
   const rpc = new CompanionRpcPeer({ transport, requestTimeoutMs, logger: logger || console });
-  const agentRuntime = new DesktopBridgeAgentRuntime({ rpc, clock: options.clock || (() => Date.now()) });
-  const host = new CompanionDesktopHost({ ...options, paths: resolvedPaths, agentRuntime, logger });
+  const agentRuntime = new DesktopBridgeAgentRuntime({ rpc, clock });
+  const host = new CompanionDesktopHost({ ...options, paths: resolvedPaths, agentRuntime, logger, migrationBootResult });
   await host.init();
   return host;
 }
