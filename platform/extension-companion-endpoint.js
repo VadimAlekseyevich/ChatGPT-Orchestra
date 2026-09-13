cf31b22354d31eada30d288d1aa83e6af502f312
Vadim <shilin.vadim.alekseyevich@gmail.com>
2026-09-13 20:07:23 +0300

    Phase 16: proxy extension UI messages to desktop

--- platform/extension-companion-endpoint.js
+++ platform/extension-companion-endpoint.js
@@ -71,6 +71,14 @@
       });
     }
 
+    async forwardLegacyMessage(message, sender = {}) {
+      if (!this.started) throw new Error("extension_companion_not_started");
+      return this.rpc.request("orchestrator.legacyMessage", {
+        message,
+        sender: this.agentRuntime.normalizeSender(sender)
+      });
+    }
+
     async forwardSessionRemoved(sessionId) {
       if (!this.started) throw new Error("extension_companion_not_started");
       return this.rpc.request("orchestrator.sessionRemoved", { sessionId: String(sessionId ?? "") });
